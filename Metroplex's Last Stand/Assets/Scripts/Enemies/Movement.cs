9e8c11d5b018cd09ddac372e68182f0dce78bd2a
KoningSherwood <ramonzwiep@hotmail.com>
2020-09-25 12:17:29 +0200

    Movement Script + Update
    Added Movement Script along with updating from 2019.3.0f3 to 2019.3.0f6.
    The reason for this being that when i tried downloading f3 online, i couldn't it automatically downloads f6..

---
+++ Metroplex's Last Stand/Assets/Scripts/Enemies/Movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Transform target;
+    public Transform cp1;
+    public Transform cp2;
+    public Transform cp3;
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, target.position) < 0.1f)
+        {
+            if (target == cp3)
+            {
+                Destroy(gameObject);
+            }
+
+            if (target == cp2)
+            {
+                target = cp3;
+            }
+
+            if (target == cp1)
+            {
+                target = cp2;
+            }
+        }
+
+        transform.LookAt(target);
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+    }
+}
