458c1b60f7a56431a3c9b896b3c39f66d3e8ef3c
JonasLK <49680607+JonasLK@users.noreply.github.com>
2020-09-28 20:00:15 +0200

    tower health and half of buff turret

---
+++ Metroplex's Last Stand/Assets/Scripts/Towers/BuffTower/AddToBuffTurret.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddToBuffTurret : MonoBehaviour
+{
+    public string buffTurretName;
+
+    public void OnTriggerEnter(Collider o)
+    {
+        if(o.gameObject.tag == buffTurretName)
+        {
+            o.gameObject.GetComponent<BuffTowers>().turretsInRange.Add(gameObject);
+        }
+    }
+
+    public void OnTriggerExit(Collider o)
+    {
+        if (o.gameObject.tag == buffTurretName)
+        {
+            o.gameObject.GetComponent<BuffTowers>().turretsInRange.Remove(gameObject);
+        }
+    }
+}
